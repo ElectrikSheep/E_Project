0fab2b79cd64866771b689ca4d619be5c3936609
Lionel Mille <lmille@ad-dispatch.com>
2013-11-16 14:37:28 -0400

    Added menu scene + some new UI

---
+++ Game/Assets/2) Script/UI_Script/Load_Scene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Load_Scene : MonoBehaviour {
+
+		[SerializeField] private string sceneName ;
+
+		private void OnPress( bool isPressed ){
+				if(isPressed ) return ;
+
+				Debug.Log("Pressed");
+				Application.LoadLevel( sceneName );	
+		}
+}
